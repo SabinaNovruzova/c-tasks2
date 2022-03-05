bf62e9ac26a0e8bc981b3560ab30e7e146c71ddd
Sabina Novruzova <tu7cldm19@aztu.local>
2022-03-05 14:13:52 +0400

    first commit

---
+++ task1/task1/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace task1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] nums = { 23,22, 3, 50, 4, 30, 45 };
+            int min = nums.Length;
+            for (int i = 1; i < nums.Length-1; i++)
+            {
+                if (min>nums[i])
+                {
+                    min = nums[i];
+                    Console.WriteLine(min);
+                }
+            }
+            Console.WriteLine("minimum ededdir");
+            
+        }
+    }
+}
---
+++ task2/task2/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace task2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string names = ("Salam AzTU");
+            bool p205 = true;
+            for (int i = 0; i < names.Length; i++)
+            {
+                if (names[i]=='a') 
+                {
+                    Console.WriteLine("a-simvolu var");
+                    p205 = false;
+                    break;
+                }
+            }
+            if (p205)
+            {
+                    Console.WriteLine("a-simvolu tapilmadi");
+            }
+            
+        }
+    }
+}
